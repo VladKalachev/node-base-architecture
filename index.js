973acd84631565e8da7bb16d5595452e1a5ce58e
Corvax <vlvete1990@yandex.ru>
2024-04-21 21:58:33 +0300

    feature: error handler

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ import { userRouter } from './users/users.js';
 const PORT = 8000;
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("Время " + Date.now())
+  next();
+});
+
 app.get('/hello', (req, res) => {
   res.end("Привет");
 })
 
 app.use('/users', userRouter)
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+})
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
