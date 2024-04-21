b6af0a2d3a15a9d6ddc5491ac050ba0b5b9ce3a2
Corvax <vlvete1990@yandex.ru>
2024-04-21 22:50:36 +0300

    chore: add tsconfig

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { userRouter } from './users/users.js';
 
 const PORT = 8000;
 const app = express();
 
-app.use((req, res, next) => {
+app.use((req: Request, res:Response, next: NextFunction) => {
   console.log("Время " + Date.now())
   next();
 });
 
-app.get('/hello', (req, res) => {
+app.get('/hello', (req: Request, res:Response) => {
   res.end("Привет");
 })
 
 app.use('/users', userRouter)
 
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res:Response, next: NextFunction) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 })
