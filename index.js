97a97867704bd066e0dc2d194b2de5fefc03f358
Corvax <vlvete1990@yandex.ru>
2024-04-21 21:45:08 +0300

    feature: add routers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const PORT = 8000;
 const app = express();
@@ -6,6 +7,8 @@ const app = express();
 app.get('/hello', (req, res) => {
   res.end("Привет");
 })
+
+app.use('/users', userRouter)
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
