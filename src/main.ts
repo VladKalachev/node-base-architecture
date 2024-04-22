0ac548fbceaf7aa4534fe335af3c7e9ec45dae70
Corvax <vlvete1990@yandex.ru>
2024-04-22 21:25:52 +0300

    chore: add reflect-metadata

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,36 @@
+import { Container, ContainerModule, interfaces } from 'inversify';
 import { App } from './app';
 import { ExeptionFilter } from './errors/exeption.filter';
 import { LoggerService } from './logger/logger.service';
 import { UserController } from './users/users.controller';
+import { ILogger } from './logger/logger.interface';
+import { IExeptionFilter } from './errors/exeption.filter.interface';
+import { IUserController } from './users/users.controller.interface';
+import { TYPES } from './types';
+import 'reflect-metadata';
 
-async function bootstrap() {
-	const logger = new LoggerService();
-	const app = new App(logger, new UserController(logger), new ExeptionFilter(logger));
+export interface IBootstrapReturn {
+	appContainer: Container;
+	app: App;
+}
+
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+	bind<ILogger>(TYPES.ILogger).to(LoggerService).inSingletonScope();
+	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+	bind<IUserController>(TYPES.UserController).to(UserController);
+	// bind<IUserService>(TYPES.UserService).to(UserService);
+	// bind<PrismaService>(TYPES.PrismaService).to(PrismaService).inSingletonScope();
+	// bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
+	// bind<IUsersRepository>(TYPES.UsersRepository).to(UsersRepository).inSingletonScope();
+	bind<App>(TYPES.Application).to(App);
+});
+
+async function bootstrap(): Promise<IBootstrapReturn> {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
 	await app.init();
+	return { appContainer, app };
 }
 
 export const boot = bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,27 +1,31 @@
-import { NextFunction, Response, Request } from "express";
-import { BaseController } from "../common/base.controller";
-import { LoggerService } from "../logger/logger.service";
-import { IUserController } from "./users.controller.interface";
+import 'reflect-metadata';
+import { NextFunction, Response, Request } from 'express';
+import { BaseController } from '../common/base.controller';
+import { IUserController } from './users.controller.interface';
+import { inject, injectable } from 'inversify';
+import { ILogger } from '../logger/logger.interface';
+import { TYPES } from '../types';
 
+@injectable()
 export class UserController extends BaseController implements IUserController {
-  constructor(logger: LoggerService) {
-    super(logger)
-    this.bindRoutes([
-      { path: '/register', method: 'post', func: this.register }, 
-      { path: '/login', method: 'post', func: this.login },
-      { path: '/info', method: 'post', func: this.info }
-    ]);
-  }
+	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+		super(loggerService);
+		this.bindRoutes([
+			{ path: '/register', method: 'post', func: this.register },
+			{ path: '/login', method: 'post', func: this.login },
+			{ path: '/info', method: 'post', func: this.info },
+		]);
+	}
 
-  login(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, 'login');
-  }
+	login(req: Request, res: Response, next: NextFunction) {
+		this.ok(res, 'login');
+	}
 
-  register(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, 'register');
-  }
+	register(req: Request, res: Response, next: NextFunction) {
+		this.ok(res, 'register');
+	}
 
-  info(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, 'info');
-  }
+	info(req: Request, res: Response, next: NextFunction) {
+		this.ok(res, 'info');
+	}
 }
